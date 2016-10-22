62292c1e970101d279215f900384a228c23a76e3
Satyapriya Krishna <krishna.satyapriya123@gmail.com>
2016-10-22 01:04:03 -0400

    Wrote Setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pysky',
+      version='0.1',
+      description='Simple Regex operations',
+      url='https://github.com/y12uc231/pysky',
+      author='spk',
+      author_email='satyaprk@andrew.cmu.edu',
+      license='MIT',
+      packages=['pysky'],
+      zip_safe=False)
