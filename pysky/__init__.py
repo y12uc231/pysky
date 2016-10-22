48c69380568f90e338c96f41bcd13fdc1dbe6546
Satyapriya Krishna <satyapriyakrishna@Satyapriyas-MBP.wv.cc.cmu.edu>
2016-10-22 01:46:45 -0400

    Added path

--- pysky/__init__.py
+++ pysky/__init__.py
@@ -1,4 +1,4 @@
 import sys
 from .refine import refine_string
 from .parser import pyparser
-sys.path.insert(0, "/Users/satyapriyakrishna/PyCharmProjects/Packages_def/pysky")
+sys.path.insert(0, "/Users/satyapriyakrishna/PyCharmProjects/Packages_def/pysky/pysky")
