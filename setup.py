c8148b97efaf502a25d0091d8bec4041b05911b2
Satyapriya Krishna <satyapriyakrishna@Satyapriyas-MBP.wv.cc.cmu.edu>
2016-10-22 01:14:57 -0400

    Added setup bug

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from setuptools import setup
 
 setup(name='pysky',
@@ -10,16 +9,3 @@ setup(name='pysky',
       license='MIT',
       packages=['pysky'],
       zip_safe=False)
-=======
-from setuptools import setup
-
-setup(name='pysky',
-      version='0.1',
-      description='Simple Regex operations',
-      url='https://github.com/y12uc231/pysky',
-      author='spk',
-      author_email='satyaprk@andrew.cmu.edu',
-      license='MIT',
-      packages=['pysky'],
-      zip_safe=False)
->>>>>>> 62292c1e970101d279215f900384a228c23a76e3
